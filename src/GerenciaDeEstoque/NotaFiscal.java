e1919883ebc545e406b44298959a3836df445a3d
Thiago Melo <tmelo387@gmail.com>
2021-03-09 18:37:17 -0300

    Att

--- src/GerenciaDeEstoque/NotaFiscal.java
+++ src/GerenciaDeEstoque/NotaFiscal.java
@@ -8,91 +8,99 @@ package GerenciaDeEstoque;
 import java.util.ArrayList;
 
 public class NotaFiscal {
-   private final int codigo;
-   private String data;
-   private double valorTotal;
-   private ArrayList<Item> relacaoItens = new ArrayList<>();
-   private static int nrCodigo = 0;
-   
+	private final int codigo;
+	private String data;
+	private double valorTotal;
+	private ArrayList<Item> relacaoItens = new ArrayList<>();
+	private static int nrCodigo = 0;
 
-   
-   public NotaFiscal(String data){
-       codigo = 1 + nrCodigo;
-       NotaFiscal.nrCodigo++;
-       this.data = data;
-   }
-   public NotaFiscal(){
-       this.codigo = 0;
-   }
-   
-   public Item getItem(int codigo) {
-	   for(Item i: relacaoItens) {
-		   if(i.getP().getCodigo() == codigo) {
-			   return i;
-		   }
-	   }
-	   return null;
-   }
-   
-   
-    public boolean addItem(Item i){
-        if(i.getP().subQuantidade(i.getQuantidade())){
-        relacaoItens.add(i);
-        return true;
-        }
-        return false;
-    }
-    
-    public boolean removeItem(Item i){
-        if(i.getP().addQuantidade(i.getQuantidade())){
-        relacaoItens.remove(i);
-        return true;
-        }
-        return false;
-    }
+	public NotaFiscal(String data) {
+		codigo = 1 + nrCodigo;
+		NotaFiscal.nrCodigo++;
+		this.data = data;
+	}
 
-    public String toString() {
-       String itens = "Código: " + codigo + "\nData: " + data + "\n";
+	public NotaFiscal() {
+		this.codigo = 0;
+	}
 
-       for (Item i : relacaoItens) {
-           itens += i.toString() + "\n";
-       }
-       itens += "Total da nota: " + getValorTotal();
+	public Item getItem(int codigo) {
+		for (Item i : relacaoItens) {
+			if (i.getP().getCodigo() == codigo) {
+				return i;
+			}
+		}
+		return null;
+	}
 
-       return itens;
-    }
+	public boolean addItem(Item i) {
+		if (i.getP().subQuantidade(i.getQuantidade())) {
+			relacaoItens.add(i);
+			return true;
+		}
+		return false;
+	}
 
+	public boolean removeItem(Item i) {
+		if (i.getP().addQuantidade(i.getQuantidade())) {
+			relacaoItens.remove(i);
+			return true;
+		}
+		return false;
+	}
 
-    public int getCodigo() {
-        return codigo;
-    }
+	public boolean removeTodosItens() {
+		for (Item i : relacaoItens) {
+			removeItem(i);
+		}
+		if (relacaoItens.size() > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-    public void setData(String data) {
-        this.data = data;
-    }
+	public String toString() {
+		String itens = "Código: " + codigo + "\nData: " + data + "\n";
 
-    public double getValorTotal() {
-       double valorTotal = 0;
-       for (Item i : relacaoItens) {
-           valorTotal += i.getValorTotal();
-       }
-        return valorTotal;
-    }
+		for (Item i : relacaoItens) {
+			itens += i.toString() + "\n";
+		}
+		itens += "Total da nota: " + getValorTotal();
 
-    public ArrayList<Item> getRelacaoItens() {
-        return relacaoItens;
-    }
+		return itens;
+	}
 
-    public String getData() {
-        return data;
-    }
+	public int getCodigo() {
+		return codigo;
+	}
 
-    public void setValorTotal(double valorTotal) {
-        this.valorTotal = valorTotal;
-    }
+	public void setData(String data) {
+		this.data = data;
+	}
 
-    public void setRelacaoItens(ArrayList<Item> relacaoItens) {
-        this.relacaoItens = relacaoItens;
-    }
-   
+	public double getValorTotal() {
+		double valorTotal = 0;
+		for (Item i : relacaoItens) {
+			valorTotal += i.getValorTotal();
+		}
+		return valorTotal;
+	}
+
+	public ArrayList<Item> getRelacaoItens() {
+		return relacaoItens;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public void setRelacaoItens(ArrayList<Item> relacaoItens) {
+		this.relacaoItens = relacaoItens;
+	}
+
 }

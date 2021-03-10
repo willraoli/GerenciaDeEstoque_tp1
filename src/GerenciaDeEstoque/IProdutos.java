cbcaba06fb6a5fdeb2fcb73662f4479b07c5ad42
William Oliveira <48337511+willraoli@users.noreply.github.com>
2021-03-10 13:50:49 -0300

    ...

--- src/GerenciaDeEstoque/IProdutos.java
+++ src/GerenciaDeEstoque/IProdutos.java
@@ -10,21 +10,21 @@ public interface IProdutos {
      * @param p Produto
      * @return True se o produto for inv=cluído com sucesso e False caso contrário.
      */
-    public abstract boolean addProduto(Produto p);
+    boolean addProduto(Produto p);
     
     /**
      * Remove o produto com código informado.
      * @param codigo Código do produto a ser removido.
      * @return True se o produto for removido com seucesso e False caso contrário.
      */
-    public abstract boolean removeProduto(int codigo);
+    boolean removeProduto(int codigo);
     
     /**
      * Captura o produto com o código informado.
      * @param codigo Código do produto a ser capturado.
      * @return O produto com o código informado ou NULL caso produto não seja encontrado.
      */
-    public abstract Produto getProduto(int codigo);
+    Produto getProduto(int codigo);
     
     /**
      * Altera a quantidade do produto com o código informado.
@@ -32,7 +32,7 @@ public interface IProdutos {
      * @param nova Nova quantidade do produto.
      * @return True se a quantidade foi corretamente atualizada e False caso contrário.
      */
-    public abstract boolean updateQuantidade(int codigo, double nova);
+    boolean updateQuantidade(int codigo, double nova);
     
 	
     /**
@@ -41,7 +41,7 @@ public interface IProdutos {
      * @param novo Novo preço do produto.
      * @return True se o preço foi corretamente atualizado e False caso contrário.
      */
-    public abstract boolean updatePreco(int codigo, double novo);
+    boolean updatePreco(int codigo, double novo);
     
     /**
      * Adiciona a quantidade ao produto com código informado.
@@ -49,7 +49,7 @@ public interface IProdutos {
      * @param quantidade Quantidade a ser acrescentada.
      * @return True se a quantidade for adicionada e False caso contrário. 
      */
-    public abstract boolean addQuantidade(int codigo, double quantidade);
+    boolean addQuantidade(int codigo, double quantidade);
 
     /**
      * Subtrai a quantidade do produto com código informado.
@@ -57,6 +57,6 @@ public interface IProdutos {
      * @param quantidade Quantidade a ser subtraída.
      * @return True se a quantidade for subtraída e False caso contrário. 
      */
-    public abstract boolean subQuantidade(int codigo, double quantidade);
+    boolean subQuantidade(int codigo, double quantidade);
 
 }
